949bc967b00d702700621b139ea5e0828a2f09d1
Aman Kumar Gupta <amanhkdigiverse@gmail.com>
2026-03-10 14:35:01 +0530

    few update in sign in page

--- lib/modules/auth/views/sign_in.dart
+++ lib/modules/auth/views/sign_in.dart
@@ -14,39 +14,63 @@ class SignIn extends StatelessWidget {
               children: [
                 Image.asset(
                   Images.splashBg,
-                  height: 200,
+                  fit: BoxFit.cover,
+                  height: 300,
                   width: double.infinity,
                 ),
-                Center(
-                  child: Text(
-                    "AI Setu",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Positioned(
+                  top: 120,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      "AI Setu",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
+            SizedBox(height: 50),
 
             // Sign In
-            Text(
-              "Welcome Back",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Sign in to your account",
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 10,
+                children: [
+                  Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Sign in to your account",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+
+                  Text("Email", style: TextStyle(fontSize: 14)),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your Password",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
+            SizedBox(),
             ElevatedButton(onPressed: () {}, child: Text("Sign In")),
           ],
         ),
