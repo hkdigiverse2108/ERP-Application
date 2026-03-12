8c99e27b3515c19d44d8e4d17929431580421689
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-03-12 16:07:47 +0530

    Creating constant variable for better usage of colors and other

--- lib/modules/home/widgets/dashboard_stat_widget.dart
+++ lib/modules/home/widgets/dashboard_stat_widget.dart
@@ -9,8 +9,28 @@ class DashboardStatWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => GridView(
+    return Obx(() {
+      final selseColor = context.responsive(
+        light: AppColors.lightSectionSell,
+        dark: AppColors.darkSectionSell,
+      );
+
+      final purchaseColor = context.responsive(
+        light: AppColors.lightSectionSellPurchase,
+        dark: AppColors.darkSectionSellPurchase,
+      );
+
+      final transectionColor = context.responsive(
+        light: AppColors.lightSectionPaid,
+        dark: AppColors.darkSectionPaid,
+      );
+
+      final profitColor = context.responsive(
+        light: AppColors.lightSectionProfit,
+        dark: AppColors.darkSectionProfit,
+      );
+
+      return GridView(
         shrinkWrap: true,
         physics: const BouncingScrollPhysics(),
         padding: EdgeInsets.zero,
@@ -21,225 +41,135 @@ class DashboardStatWidget extends StatelessWidget {
           mainAxisSpacing: 16.0,
         ),
         children: [
-          StatCard(
-            title: "Total Sales",
-            value: 0,
-            tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
-          ),
+          StatCard(title: "Total Sales", value: 0, tag: "₹", color: selseColor),
           StatCard(
             title: "Total Invoice",
             value: 0,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
+            color: selseColor,
           ),
-          StatCard(
-            title: "Sold Qty",
-            value: 0,
-            tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
-          ),
+          StatCard(title: "Sold Qty", value: 0, tag: "₹", color: selseColor),
           StatCard(
             title: "Total Customer",
             value: 4,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
+            color: selseColor,
           ),
-          StatCard(
-            title: "To Receive",
-            value: 0,
-            tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
-          ),
+          StatCard(title: "To Receive", value: 0, tag: "₹", color: selseColor),
           StatCard(
             title: "Total Sales Return",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSell,
-              dark: AppColors.darkSectionSell,
-            ),
+            color: selseColor,
           ),
 
           StatCard(
             title: "Total Purchase",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
+            color: purchaseColor,
           ),
           StatCard(
             title: "Total Bills",
             value: 0,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
+            color: purchaseColor,
           ),
           StatCard(
             title: "Purchase Qty",
             value: 0,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
+            color: purchaseColor,
           ),
           StatCard(
             title: "Total Suppliers",
             value: 0,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
+            color: purchaseColor,
           ),
-          StatCard(
-            title: "To Pay",
-            value: 0,
-            tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
-          ),
+          StatCard(title: "To Pay", value: 0, tag: "₹", color: purchaseColor),
           StatCard(
             title: "Total Purchase Return",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionSellPurchase,
-              dark: AppColors.darkSectionSellPurchase,
-            ),
+            color: purchaseColor,
           ),
+
           StatCard(
             title: "Total Paid",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
           StatCard(
             title: "Total Expense",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
           StatCard(
             title: "Total Products",
             value: 43,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
           StatCard(
             title: "Stock Qty",
             value: 56,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
           StatCard(
             title: "Stock Value",
             value: 100,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
           StatCard(
             title: "Cash in Hand",
             value: 100,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionPaid,
-              dark: AppColors.darkSectionPaid,
-            ),
+            color: transectionColor,
           ),
+
           StatCard(
             title: "Gross Profit",
             value: 100,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
           StatCard(
             title: "Avg. Profit Margin",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
           StatCard(
             title: "Avg. Profit margin (%)",
             value: 0,
             // tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
           StatCard(
             title: "Avg. Cart Value",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
           StatCard(
             title: "Avg. Bills (Nos.)",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
           StatCard(
             title: "Bank Access",
             value: 0,
             tag: "₹",
-            color: context.responsive(
-              light: AppColors.lightSectionProfit,
-              dark: AppColors.darkSectionProfit,
-            ),
+            color: profitColor,
           ),
         ],
-      ),
-    );
+      );
+    });
   }
 }
