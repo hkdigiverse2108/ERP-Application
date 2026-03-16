52e25106d518b971da80fddfde5bbfa3f0c4e37e
Aman Kumar Gupta <amanhkdigiverse@gmail.com>
2026-03-16 12:01:54 +0530

    create user section page

--- lib/modules/user/controllers/user_controller.dart
+++ lib/modules/user/controllers/user_controller.dart
@@ -1,30 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserController extends GetxController {
-  final nameController = TextEditingController();
+  final fullNameController = TextEditingController();
+  final userNameController = TextEditingController();
   final emailController = TextEditingController();
   final phoneController = TextEditingController();
+  final passwordController = TextEditingController();
+  final userDescriptionController = TextEditingController();
+  final roleController = TextEditingController();
+  final panController = TextEditingController();
+  final branchController = TextEditingController();
+  // Address Details
   final addressController = TextEditingController();
   final cityController = TextEditingController();
   final stateController = TextEditingController();
   final zipCodeController = TextEditingController();
   final countryController = TextEditingController();
-  final passwordController = TextEditingController();
-  final confirmPasswordController = TextEditingController();
+  // Bank Details
+  final bankNameController = TextEditingController();
+  final accountNumberController = TextEditingController();
+  final ifscCodeController = TextEditingController();
+  final bankBranchController = TextEditingController();
+  // Salary Details
+  final wagesController = TextEditingController();
+  final commissionController = TextEditingController();
+  final extraWagesController = TextEditingController();
+  final targetController = TextEditingController();
 
   @override
   void onClose() {
-    nameController.dispose();
+    fullNameController.dispose();
+    userNameController.dispose();
     emailController.dispose();
     phoneController.dispose();
+    passwordController.dispose();
+    userDescriptionController.dispose();
+    roleController.dispose();
+    panController.dispose();
+    branchController.dispose();
+
+    // Address Details
     addressController.dispose();
     cityController.dispose();
     stateController.dispose();
     zipCodeController.dispose();
     countryController.dispose();
-    passwordController.dispose();
-    confirmPasswordController.dispose();
     super.onClose();
+    //Bank Detaila
+    bankNameController.dispose();
+    accountNumberController.dispose();
+    ifscCodeController.dispose();
+    bankBranchController.dispose();
+    // Salary Details
+    wagesController.dispose();
+    commissionController.dispose();
+    extraWagesController.dispose();
+    targetController.dispose();
   }
+
+  // Image Picker
+  final ImagePicker _picker = ImagePicker();
 }
