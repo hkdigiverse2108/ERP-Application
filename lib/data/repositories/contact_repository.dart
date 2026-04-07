277a1005acfd936c8438bd16bfc61701d2894fc2
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-04-07 12:10:30 +0530

    feat: initialize API constants and add contact repository for data management

--- lib/data/repositories/contact_repository.dart
+++ lib/data/repositories/contact_repository.dart
@@ -1,6 +1,7 @@
 import 'package:ai_setu/core/constants/api_constants.dart';
 import 'package:ai_setu/core/services/api_servicess.dart';
 import 'package:ai_setu/data/model/contact_model/contact_model.dart';
+import 'package:ai_setu/data/model/res/res_model.dart';
 import 'package:get/get.dart';
 
 class ContactRepository {
@@ -11,13 +12,16 @@ class ContactRepository {
     int limit = 10,
     String? search,
   }) async {
-    final response = await _apiService.get(
+    final ResModel response = await _apiService.get(
       ApiConstants.buildUrl(ApiConstants.getAllContact, {
         "page": page,
         "limit": limit,
         "search": search,
       }),
     );
-    return ContactListModel.fromJson(response);
+    if (response.status == 200 && response.data != null) {
+      return ContactListModel.fromJson(response.data);
+    }
+    throw Exception(response.message ?? 'Failed to load contacts');
   }
 }
